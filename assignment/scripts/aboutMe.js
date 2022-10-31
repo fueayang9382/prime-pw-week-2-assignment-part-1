580893901a45bba018bd23aa39a13508cad64f1b
Tou Fue Yang <touyangbiz@gmail.com>
2022-10-31 13:49:40 -0500

    add first and last name

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,11 +1,12 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+const firstname="Tou";
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+const lastName="Yang";
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+const fullName= firstname + lastName; 
+console.log(fullName);
 // 4 - Console log the value of `fullName`
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
